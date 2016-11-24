d9be5dab787840d23e772c5af2f13dfd35361f00
Bas Wegh <bas.wegh@kit.edu>
2016-11-24 15:09:14 +0100

    fix issue with decoding base64 userid

--- plugin/info.py
+++ plugin/info.py
@@ -8,6 +8,7 @@ import random
 import string
 import sys
 import traceback
+import time
 
 def list_params():
     RequestParams = []
@@ -15,6 +16,7 @@ def list_params():
     return json.dumps({'result':'ok', 'conf_params': ConfParams, 'request_params': RequestParams, 'version':'dev'})
 
 def request_info(JObject):
+    time.sleep(2)
     Credential = info_to_credential(JObject)
     return json.dumps({'result':'ok', 'credential': Credential, 'state': 'user_info'})
 
@@ -22,7 +24,7 @@ def info_to_credential(JObject):
     Version = JObject['tts_version']
     UserId = JObject['tts_userid']
     UserInfo = JObject['user_info']
-    UserId_data = str(UserId) + '=' * (4 - len(sys.argv[1]) % 4)
+    UserId_data = str(UserId)+ '=' * (4 - len(UserId) % 4)
     DecodedUserId = str(base64.urlsafe_b64decode(UserId_data))
     OidcCredential = [
         {'name':'TTS version', 'type':'text', 'value':Version},
@@ -64,29 +66,32 @@ def oidc_key_to_type(Key):
     return "text"
 
 def revoke_info():
+    time.sleep(1)
     return json.dumps({'result': 'ok'})
 
 
 def main():
     try:
+        UserMsg = "Internal error, please contact the administrator"
+
         if len(sys.argv) == 2:
-            json_data = str(sys.argv[1]) + '=' * (4 - len(sys.argv[1]) % 4)
-            jobject = json.loads(str(base64.urlsafe_b64decode(json_data)))
-            action = jobject['action']
-            if action == "parameter":
+            Json = str(sys.argv[1])+ '=' * (4 - len(sys.argv[1]) % 4)
+            JObject = json.loads(str(base64.urlsafe_b64decode(Json)))
+            Action = JObject['action']
+            if Action == "parameter":
                 print list_params()
+            elif Action == "request":
+                print request_info(JObject)
+            elif Action == "revoke":
+                print revoke_info()
             else:
-                if action == "request":
-                    print request_info(jobject)
-                elif action == "revoke":
-                    print revoke_info()
-                else:
-                    print json.dumps({"error": "unknown_action", "details": action})
+                print json.dumps({"error": "unknown_action", "details": action})
         else:
             print json.dumps({"error": "no_parameter"})
     except Exception, E:
         TraceBack = traceback.format_exc(),
-        print json.dumps({"error": "exception", "details": str(E), "trace": TraceBack})
+        LogMsg = "the plugin failed with %s - %s"%(str(E), TraceBack)
+        print json.dumps({'result':'error', 'user_msg':UserMsg, 'log_msg':LogMsg})
 
 if __name__ == "__main__":
     main()
