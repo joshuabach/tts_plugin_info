6eaa4376f5f2991d1fb70c85c38f93c2ac585db1
Joshua Bachmeier <uwdkl@student.kit.edu>
2017-04-05 14:39:42 +0200

    Wrap plugin tester setup into function

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -9,6 +9,15 @@
 : ${PLUGIN_TESTER_REPO:="https://github.com/indigo-dc/watts-plugin-tester.git"}
 
 
+setup_plugin_tester() {
+    rm -rf /tmp/plugin-tester
+    git clone $PLUGIN_TESTER_REPO /tmp/plugin-tester || exit
+
+    pushd /tmp/plugin-tester || exit
+    go build || exit
+    popd
+}
+
 test_plugin() {
     echo ${2:-'null'} > /tmp/plugin_input || return
     args="test plugin/info.py --plugin-action=$1 --json=/tmp/plugin_input"
@@ -17,13 +26,7 @@ test_plugin() {
 }
 
 
-# Get and build the plugin tester
-rm -rf /tmp/plugin-tester
-git clone $PLUGIN_TESTER_REPO /tmp/plugin-tester || exit
-
-pushd /tmp/plugin-tester || exit
-go build || exit
-popd
+setup_plugin_tester
 
 # TODO More tests than one per action?
 test_plugin parameter "$PLUGIN_PARAMETER_INPUT" || exit
